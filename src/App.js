afe2de34c7b3fbc8f09b64e9662b3bba0255d43e
Kumar Neeraj <33743260+KNeeraj038@users.noreply.github.com>
2021-04-30 19:19:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+// var userName = prompt("Give me your name");
+// var txtColor = "blue";
+
+// var likeCounter = 0;
+var emojiDictionary = {
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    // console.log(event.target);
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === "undefined") {
+      meaning = "We don't have it in our database !";
+    } else {
+      setMeaning(meaning);
+    }
+    // console.log(meaning);
+  }
+
+  return (
+    <div className="App">
+      <input onChange={emojiInputHandler}></input>
+      <p> Meaning: {meaning}</p>
+    </div>
+  );
+}
